e57646f63a3d0c816b93bf0cc0f49d81ec7f4099
sudhir-kumar999 <sk2001may@gmail.com>
2026-02-04 17:31:33 +0530

    padding

--- client/src/pages/user/MyAppointments.jsx
+++ client/src/pages/user/MyAppointments.jsx
@@ -95,7 +95,7 @@ const MyAppointments = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
               </svg>
             </div>
-            <h3 className="text-xl font-semibold text-gray-900 mb-2">
+            <h3 className="text-xl font-semibold text-gray-900 mb-2 lg:mt-8">
               No Appointments Found
             </h3>
             <p className="text-gray-600">
