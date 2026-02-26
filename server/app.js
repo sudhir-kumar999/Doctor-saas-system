267a53b4ae1221c41761c9536be3cba184843085
sudhir-kumar999 <sk2001may@gmail.com>
2026-02-26 10:56:41 +0530

    proxy

--- server/app.js
+++ server/app.js
@@ -14,6 +14,7 @@ import webhookRoutes from "./src/routes/webhookRoutes.js";
 
 
 const app = express();
+app.set("trust proxy", 1);
 app.use(
   "/api/webhook",
   express.raw({ type: "application/json" }),
